b617dc7cdc50a42228cfd5b8ad2a8be29fa4f6b7
nanospearing <spearingnathanial@gmail.com>
2023-12-24 14:24:15 -0500

    Added High Score Feature

--- src/app/game/tetris.js
+++ src/app/game/tetris.js
@@ -25,22 +25,26 @@ function useInterval(callback, delay) {
 export function TetrisDiv() {
 
   const setCookie = (name, value, days) => {
-    const expires = new Date();
-    expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
-    document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+    if (typeof document !== 'undefined') {
+      const expires = new Date();
+      expires.setTime(expires.getTime() + days * 24 * 60 * 60 * 1000);
+      document.cookie = `${name}=${value};expires=${expires.toUTCString()};path=/`;
+    }
   };
 
 
   const getCookie = (name) => {
-    const cookieName = `${name}=`;
-    const cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-      let cookie = cookies[i].trim();
-      if (cookie.indexOf(cookieName) === 0) {
-        return cookie.substring(cookieName.length, cookie.length);
+    if (typeof document !== 'undefined') {
+      const cookieName = `${name}=`;
+      const cookies = document.cookie.split(';');
+      for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) === 0) {
+          return cookie.substring(cookieName.length, cookie.length);
+        }
       }
+      return '';
     }
-    return '';
   };
 
 
