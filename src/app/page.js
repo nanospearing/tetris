cf83c563d60626f1f64dac171f17a01dfdc179a6
nanospearing <spearingnathanial@gmail.com>
2023-12-26 10:03:20 -0500

    Add accordion component for game instructions and references

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,9 @@
 'use client'
 
 import React from "react";
-import { BrowserView, MobileView } from 'react-device-detect'; 
+import { BrowserView, MobileView } from 'react-device-detect';
 import Image from "next/image";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button } from "@nextui-org/react";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Accordion, AccordionItem } from "@nextui-org/react";
 import { GithubIcon, ControllerIcon } from "./components/svgs"
 
 
@@ -38,6 +38,32 @@ export default function Page() {
           <div className="flex flex-col items-center justify-center space-y-6">
             <Link className="mt-5" href="/tetris/play"><Button color="success" size="lg" endContent={<ControllerIcon />}>Start Playing!</Button></Link>
             <Link href="https://github.com/nanospearing/tetris"><Button color="secondary" size="lg" endContent={<GithubIcon />}>Get the source code!</Button></Link>
+          </div>
+          <div className="w-10/12">
+            <Accordion variant="shadow">
+              <AccordionItem key="1" title="How to Play and Controls">
+                <p className="font-bold text-medium">How to Play: </p>
+                <div class>
+                  <p>You can check out a official guide</p>
+                  <Link href="https://tetris.com/article/33/tetris-tips-for-beginners">here</Link>
+                </div>
+                <p className="font-bold text-medium">Controls: </p>
+                <p>- Use the up arrow to turn your block</p>
+                <p>- Use the down arrow to make the block fall quicker</p>
+                <p>- Use the left and right arrow to move the block left and right</p>
+                <p>- Use the spacebar to hard drop the block (Drops it all the way to the bottom)</p>
+                <p>- Use the p key to pause and unpause the game</p>
+              </AccordionItem>
+              <AccordionItem key="2" title="References">
+                <p>-  https://tetris.wiki/Scoring (Scoring based off of Original BPS)</p>
+                <p>-  https://tetris.wiki/Tetris_Guideline (Blocks and rules)</p>
+                <p>-  Official Tetris logo for logo design</p>
+              </AccordionItem>
+              <AccordionItem key="3" title="Licensing">
+                <p>- Korobeiniki.ogg (This file has been identified as being free of known restrictions under copyright law, including all related and neighboring rights.)</p>
+                <Link href="https://commons.wikimedia.org/wiki/File:Korobeiniki.ogg">Source</Link>
+              </AccordionItem>
+            </Accordion>
           </div>
         </div>
       </BrowserView>
